093459dd39bb9e4c65ac914731b2ac6adf4fd697
Leo <leo.leduff@gmail.com>
2023-01-26 15:38:33 +0100

    Added a cost field in Plush class

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -2,5 +2,17 @@
 
 class Plush
 {
-    
+public:
+    int get_cost() const
+    {
+        return _cost;
+    }
+
+    void set_cost(int value)
+    {
+        _cost = value;
+    }
+
+private:
+    int _cost = 10;
 };
