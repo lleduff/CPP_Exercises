726a842b2db304ba56cc3c909e98830801942b07
Leo <leo.leduff@gmail.com>
2023-02-23 08:58:53 +0100

    tp 5 exercices

--- src/Card.hpp
+++ src/Card.hpp
@@ -1,1 +1,60 @@
 #pragma once
+
+#include <string>
+
+using namespace std;
+
+enum class CardType {
+    Monster,
+    Spell,
+    Trap,
+};
+
+inline string to_string(CardType type) {
+    switch (type) {
+        case CardType::Monster:
+            return "Monster";
+        case CardType::Spell:
+            return "Spell";
+        case CardType::Trap:
+            return "Trap";
+    }
+}
+
+class Card {
+    public:
+        Card(const string id, CardType type)
+            : _id { id }
+            , _type { type }
+        {}
+
+        string get_id() const {
+            return _id;
+        }
+
+        CardType get_type() const {
+            return _type;
+        }
+
+        string get_name() const {
+            return _name;
+        }
+
+        string get_description() const {
+            return _description;
+        }
+
+        void set_name(const string name) {
+            _name = name;
+        }
+
+        void set_description(const string description) {
+            _description = description;
+        }
+        
+    private:
+        string _id;
+        CardType _type;
+        string _name;
+        string _description;
+};
--- src/Monster.hpp
+++ src/Monster.hpp
@@ -1,1 +1,69 @@
 #pragma once
+
+#include <Card.hpp>
+
+using namespace std;
+
+enum class Attribute {
+    Dark,
+    Divine,
+    Earth,
+    Fire,
+    Light,
+    Water,
+    Wind,
+};
+
+inline string to_symbol(Attribute attribute) {
+    switch (attribute) {
+        case Attribute::Dark:
+            return u8"闇";
+        case Attribute::Divine: 
+            return u8"神";
+        case Attribute::Earth:
+            return u8"地";
+        case Attribute::Fire:
+            return u8"炎";
+        case Attribute::Light:
+            return u8"光";
+        case Attribute::Water:
+            return u8"水";
+        case Attribute::Wind:
+            return u8"風";
+    }
+};
+
+class Monster: public Card {
+    public:
+        Monster(const string id, const string name, Attribute attribute, const string unknown, int atk, int def)
+            : Card { id, CardType::Monster }
+            , _attribute { attribute }
+            , _unknown { unknown }
+            , _atk { atk }
+            , _def { def }
+            {
+                set_name(name);
+            }
+    
+        Attribute get_attribute() const {
+            return _attribute;
+        }
+
+        int get_atk() const {
+            return _atk;
+        }
+
+        int get_def() const {
+            return _def;
+        }
+
+        string get_description() const {
+            return "[" + _unknown + "]\n" + Card::get_description() + "\nATK/" + to_string(_atk) + " DEF/" + to_string(_def);
+        }         
+
+    private:
+        Attribute _attribute;
+        string _unknown;
+        int _atk;
+        int _def;
+};
